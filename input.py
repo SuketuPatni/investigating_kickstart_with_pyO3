6b653f25655f947f0e07674b6274050953ec7001
SuketuPatni <suketublackpanther9@gmail.com>
2022-03-05 19:02:25 +0530

    clippy linted, rustfmt formatted, code made DRY and useless comments removed

--- input.py
+++ input.py
@@ -1,53 +1,39 @@
 import ks_d1_2021
 
-# The old code to take inputs for test cases.
-
-# T = int(input())
-# answers_list = []
-# for _ in range(0, T, 1):
-    # vec_1 = [int(i) for i in input().split()]
-    # vec_2 = [int(i) for i in input().split()]
-    # vec_3 = [int(i) for i in input().split()]
-
-    # answers_list.append(ks_d1_2021.solve(vec_1, vec_2, vec_3))
-
-# for i in range(0, T, 1):
-#     print(f"Case #{i + 1}: {answers_list[i]}")
-
-
-# The new tests:-
-
 def compare(list1, list2):
     check = True
-    length = len(list1)
-    for i in range(0, length, 1):
+    for i in range(0, len(list1), 1):
         if list1[i] != list2[i]:
             check = False
             print(f"Difference in output and expected output in test case {i+1}.")
-            print(f"Output: {list1[i]}")
-            print(f"Expected output: {list2[i]}")
+            print(f"Output: {list1[i]}\nExpected output: {list2[i]}")
     if check:
         print("All tests passed successfully.")
 
 # Test Set #1
 answers_list1 = []
 
-f1 = open("test_set_1/ts1_input.txt")
-test_list = f1.read().split("\n")
-f1.close()
+with open("test_set_1/ts1_input.txt") as f1:
+    test_list_1 = f1.read().split("\n")
 
-for i in range(1, len(test_list), 3):
-    vec_1 = [int(j) for j in test_list[i].strip().split(" ")]
-    vec_2 = [int(j) for j in test_list[i + 1].strip().split(" ")]
-    vec_3 = [int(j) for j in test_list[i + 2].strip().split(" ")]
+def parse_int(string):
+    return [int(i) for i in string.strip().split(" ")]
+
+for i in range(1, len(test_list_1), 3):
+    vec_1, vec_2, vec_3 = (
+        parse_int(test_list_1[i]), 
+        parse_int(test_list_1[i + 1]), 
+        parse_int(test_list_1[i + 2])
+    )
+
     answers_list1.append(ks_d1_2021.solve(vec_1, vec_2, vec_3))
 
-# print(answers_list) # debugging
-f2 = open("test_set_1/ts1_output.txt")
-answer_key1 = [int(k[-1]) for k in f2.read().split("\n")]
-f2.close()
+# print(answers_list)
 
-# answers_list1[1] = 7 # to check testing
+with open("test_set_1/ts1_output.txt") as f2:
+    answer_key1 = [int(k[-1]) for k in f2.read().split("\n")]
+
+# answers_list1[1] = 7
 
 print("Test Set 1:-")
 compare(answers_list1, answer_key1)
@@ -55,22 +41,23 @@ compare(answers_list1, answer_key1)
 # Test Set #2
 answers_list2 = []
 
-f3 = open("test_set_2/ts2_input.txt")
-test_list2 = f3.read().split("\n")
-f3.close()
+with open("test_set_2/ts2_input.txt") as f3:
+    test_list_2 = f3.read().split("\n")
 
-for i in range(1, len(test_list2), 3):
-    vec_1 = [int(j) for j in test_list2[i].strip().split(" ")]
-    vec_2 = [int(j) for j in test_list2[i + 1].strip().split(" ")]
-    vec_3 = [int(j) for j in test_list2[i + 2].strip().split(" ")]
+for i in range(1, len(test_list_2), 3):
+    vec_1, vec_2, vec_3 = (
+        parse_int(test_list_2[i]), 
+        parse_int(test_list_2[i + 1]), 
+        parse_int(test_list_2[i + 2])
+    )
     answers_list2.append(ks_d1_2021.solve(vec_1, vec_2, vec_3))
 
-# print(answers_list) # debugging
-f4 = open("test_set_2/ts2_output.txt")
-answer_key2 = [int(k[-1]) for k in f4.read().split("\n")]
-f4.close()
+# print(answers_list) 
+
+with open("test_set_2/ts2_output.txt") as f4:
+    answer_key2 = [int(k[-1]) for k in f4.read().split("\n")]
 
 print("\nTest Set 2:-")
-# answers_list2[3] = 8 # to check testing
+# answers_list2[3] = 8
 # answers_list2[4] = 5 
 compare(answers_list2, answer_key2)
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
-use std::collections::HashMap;
 use pyo3::prelude::*;
+use std::collections::HashMap;
 
 fn mode(numbers: &[i64]) -> i64 {
     let mut occurrences = HashMap::new();
@@ -16,56 +16,62 @@ fn mode(numbers: &[i64]) -> i64 {
         .expect("Cannot compute the mode of zero numbers")
 }
 
-
 #[pyfunction]
 fn solve(row_01: Vec<i64>, row_02: Vec<i64>, row_03: Vec<i64>) -> PyResult<i64> {
+
     let mut num_aps: i64 = 0;
+
     if row_02[0] - row_01[0] == row_03[0] - row_02[0] {
         num_aps += 1;
     }
+
     if row_01[1] - row_01[0] == row_01[2] - row_01[1] {
         num_aps += 1;
     }
+
     if row_02[1] - row_01[2] == row_03[2] - row_02[1] {
         num_aps += 1;
     }
+
     if row_03[1] - row_03[0] == row_03[2] - row_03[1] {
         num_aps += 1;
     }
 
     let mut sum_vec: Vec<i64> = vec![];
+
     if (row_01[0] + row_03[2]) % 2 == 0 {
         sum_vec.push(row_01[0] + row_03[2]);
     }
+
     if (row_01[1] + row_03[1]) % 2 == 0 {
         sum_vec.push(row_01[1] + row_03[1]);
     }
+
     if (row_01[2] + row_03[0]) % 2 == 0 {
         sum_vec.push(row_01[2] + row_03[0]);
     }
+
     if (row_02[0] + row_02[1]) % 2 == 0 {
         sum_vec.push(row_02[0] + row_02[1]);
     }
-    // let sum_vec_2 = setify(&sum_vec);
-    // num_aps += (sum_vec.len() - sum_vec_2.len() + 1) as i64;
-    if sum_vec.len() == 0 {
-        num_aps += 0;
-    } else if sum_vec.len() == 1 {
-        num_aps += 1;
-    } else if sum_vec.len() == 2 {
-        if sum_vec[0] == sum_vec[1] {
-            num_aps += 2;
-        } else {
-            num_aps += 1;
+
+    num_aps += match sum_vec.len() {
+        0 => 0,
+        1 => 1,
+        2 => match sum_vec[0] == sum_vec[1] {
+            true => 2,
+            false => 1
+        },
+        _ => {
+            let mode = mode(&sum_vec);
+            // for i in sum_vec {
+            //     if i == mode {
+            //         num_aps += 1;
+            //     }
+            // }
+            sum_vec.iter().filter(|&n| *n == mode).count()
         }
-    } else {
-        let mode = mode(&sum_vec);
-        for i in sum_vec {
-            if i == mode {
-                num_aps += 1;
-            }
-        }
-    }
+    } as i64;
 
     Ok(num_aps)
 }
